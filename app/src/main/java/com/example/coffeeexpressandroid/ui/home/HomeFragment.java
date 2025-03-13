d17673f19cf88d5ad10cbe023e81f7d2f4e6cec1
Fauner Echeverri <120021440+Faebb@users.noreply.github.com>
2025-03-13 02:03:41 -0500

    feat: Agregar Home del aplicativo

--- app/src/main/java/com/example/coffeeexpressandroid/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/coffeeexpressandroid/ui/home/HomeFragment.java
@@ -21,9 +21,6 @@ private FragmentHomeBinding binding;
 
     binding = FragmentHomeBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
